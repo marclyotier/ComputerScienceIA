202aaa3fe24d26d0fab7260fafa9b73f83514c47
ArdvarkDev <ardvark8op@gmail.com>
2021-12-10 10:51:49 -0800

    whoops, I made an oopsie

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,34 @@
  
 public class Main {
 	public static void main(String[] args) throws Exception {
-		blobdetection blob = new blobdetection();
- int[][] blobs; {
+		BlobDetection blob = new BlobDetection();		
 		blob.ProcessImage();
-//	    blob.ProcessImageblue();
- 		//int xpoint = blob.x;
-		//int ypoint = blob.y;
-		 int redblobs[][];
-		 System.out.println(blob.count);
+		
+		int redblobs[][];
+		System.out.println(blob.count);
 		redblobs = new int [blob.count][2];
 		System.out.println("test 2");
-		for(int i = 1; i < blob.count-1; i++) {
+		
+		for (int i = 1; i < blob.count-1; i++) {
+			
 			System.out.println("test array count");
-		int[] bob = blob.detect();
-		redblobs [i][0] = bob[0];
-		redblobs [i][1] = bob[1];
-		System.out.println(bob[0] + "," +bob[1]);
+
+			int[] bob = blob.detect();
+			if (bob == null) {
+				System.err.println("blob.detect() returned null, I cannot continue");
+				return;
+			}
+
+			redblobs[i][0] = bob[0];
+			redblobs[i][1] = bob[1];
+			System.out.println(bob[0] + "," +bob[1]);
 		
 
-		  for(i = 0; i < blob.count; i++); {
-		  	System.out.println("test array print");
-		  	System.out.println(redblobs[i][0]);
-		  	System.out.println(redblobs[i][1]);
-		  }
-		  }
+			for(int j = 0; j < blob.count; j++) {
+				System.out.println("test array print");
+		  		System.out.println(redblobs[j][0]);
+		  		System.out.println(redblobs[j][1]);
+			}
 		}
-//		blueblobs = new int [blob.count][2];
-//		System.out.println(xpoint + ", " + ypoint);
 	}
-	}
-
-	
-
+}
